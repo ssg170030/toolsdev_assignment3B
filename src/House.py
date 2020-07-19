ecd8f70ef6e3831984b17f3e140a4647811de793
Sean Gardner <xseangx@gmail.com>
2020-07-19 15:01:18 -0500

    Working Ui init cause it inside function 7/19/2020

--- src/House.py
+++ src/House.py
@@ -1,4 +1,5 @@
 import logging
+import maya.cmds  as mc
 import pymel.core as pmc
 from pymel.core.system import Path
 
@@ -7,6 +8,33 @@ log = logging.getLogger(__name__)
 class PolyHouse(object):
 
 
-    def __index__(self):
+    def __index__(self, x = '', y= '', w= '' , h= ''):
 
-        pass
+        self.x = 10
+        self.y =10
+        self.w=10
+        self.h=10
+
+    @property
+    def x(self):
+        print('getting')
+        self.x = mc
+        return
+
+    @property
+    def y(self):
+        print('getting')
+        self.y = mc
+        return
+
+    @property
+    def w(self):
+        print('getting')
+        self.x = mc
+        return
+
+    @property
+    def h(self):
+        print('getting')
+        self.x = mc
+        return
--- src/Low_Poly_City.py
+++ src/Low_Poly_City.py
@@ -1,9 +1,10 @@
 import maya.OpenMayaUI as omui
 from PySide2 import QtWidgets, QtCore
+
 from shiboken2 import wrapInstance
 
-
 import House
+
 
 def maya_main_window():
     """Return the maya main window widget
@@ -13,35 +14,42 @@ def maya_main_window():
 
 
 class low_poly_city_ui(QtWidgets.QDialog):
-    
-    def __index__(self):
 
-        super(low_poly_city_ui, self).__index__(parent=maya_main_window())
-        House.PolyHouse()
-        self.title_lable()
+    def __init__(self):
+        super(low_poly_city_ui, self).__init__(parent=maya_main_window())
+        self.scene = House.PolyHouse()
         self.setWindowTitle("Low Poly Buildings")
+
         self.setWindowFlags(self.windowFlags() ^
                             QtCore.Qt.WindowContextHelpButtonHint)
-        self.resize(500, 300)
-        self.create_widgets()
+        self.resize(500, 200)
+        self.display_widgets()
+        self.window_layout()
         self.create_layout()
 
 
 
-    def title_lable(self):
+        #self.cmd = 'polyCone'
+
+    def display_widgets(self):
+        self.title_widget()
+        self.create_widgets()
+
+    def title_widget(self):
         self.title_lbl = QtWidgets.QLabel("LowPoly Buildings")
         self.title_lbl.setStyleSheet("font: bold 40px")
 
     def create_widgets(self):
-
-        pass
-
-
+        self.x_lbl = QtWidgets.QLabel("position")
 
     def create_layout(self):
+        self.create_layouts = QtWidgets.QHBoxLayout()
+        self.create_layouts.addWidget(self.x_lbl)
+        self.main_layout.addLayout(self.create_layouts)
+        self.setLayout(self.main_layout)
 
 
-        pass
-
-
-
+    def window_layout(self):
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.addWidget(self.title_lbl)
+        self.create_layout()
